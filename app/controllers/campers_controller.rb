f0543f390006c482feb3aabc113b10d0a444f377
EliazarAgwaya <eliazar.magero@student.moringaschool.com>
2022-10-10 05:13:14 +0300

    commit tree 018abbe6de906724f2a5adeda3b44ed8463b77c6

---
+++ app/controllers/campers_controller.rb
@@ -0,0 +1,11 @@
+class CampersController < ApplicationController
+    def index 
+        campers = Camper.all 
+        render json: campers
+    end
+
+    def show 
+        camper = Camper.find_by(id: params[:id])
+        render json: camper 
+    end
+end
